215103d068ecb41cb50d7dee72eca3629360778b
lulifa <lu.lifa@163.com>
2021-02-25 15:10:44 +0800

    新增：修改密码功能

--- RuichenCore.Api/Controllers/Auth/UserSettingController.cs
+++ RuichenCore.Api/Controllers/Auth/UserSettingController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Utility.Core;
 
 namespace RuichenCore.Api.Controllers.Auth
 {
@@ -56,6 +57,48 @@ namespace RuichenCore.Api.Controllers.Auth
             await UserService.Update(user);
             return JsonCore(true);
         }
+
+        /// <summary>
+        /// 密码设置
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ResponseResult> GetUserPassword()
+        {
+            User user = await UserService.GetSingle(CurrentUser.UserId);
+            if (user == null)
+            {
+                return JsonCore(false, "用户不存在");
+            }
+            string password = CryptoHelper.DecryptAes(user.Password);
+            return JsonCore(true, new
+            {
+                password
+            });
+        }
+
+        [HttpPost]
+        public async Task<ResponseResult> SaveUserPassword(SaveUserPasswordParam param)
+        {
+            User user = await UserService.GetSingle(CurrentUser.UserId);
+            if (user == null)
+            {
+                return JsonCore(false, "用户不存在");
+            }
+            string encryOldPWd = CryptoHelper.EncryptAes(param.OldPWD);
+            string encryNewPWD = CryptoHelper.EncryptAes(param.NewPWD);
+            if (user.Password != encryOldPWd)
+            {
+                return JsonCore(false, "原密码错误");
+            }
+            if (user.Password == encryNewPWD)
+            {
+                return JsonCore(false, "新密码与原密码不能相同");
+            }
+            user.Password = encryNewPWD;
+            await UserService.Update(user);
+            return JsonCore(true);
+        }
     }
 
     public class SaveUserInfoParam
@@ -64,5 +107,11 @@ namespace RuichenCore.Api.Controllers.Auth
         public string Mail { get; set; }
         public string Mobile { get; set; }
         public string Position { get; set; }
+    }
+
+    public class SaveUserPasswordParam
+    {
+        public string OldPWD { get; set; }
+        public string NewPWD { get; set; }
     }
 }
